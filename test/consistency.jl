72fe60fc99ccfed801a8981271e33103ec6b32b7
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-12-02 22:18:09 +0900

    use joinpath

--- test/consistency.jl
+++ test/consistency.jl
@@ -5,7 +5,7 @@
 
 info("Check consistency with the original WORLD")
 
-x = vec(readdlm("data/x.txt"))
+x = vec(readdlm(joinpath(dirname(@__FILE__), "data", "x.txt")))
 
 fs = 16000
 period = 5.0
@@ -14,7 +14,7 @@ opt = DioOption(80.0, 640.0, 2, period, 12)
 
 # Fundamental frequency (f0) estimation by DIO
 f0, timeaxis = dio(w, x; opt=opt)
-f0_org = vec(readdlm("data/f0.txt"))
+f0_org = vec(readdlm(joinpath(dirname(@__FILE__), "data", "f0.txt")))
 
 println("Maximum error in DIO is $(maximum(abs(f0-f0_org)))")
 @test length(f0) == length(f0_org)
@@ -22,7 +22,7 @@ println("Maximum error in DIO is $(maximum(abs(f0-f0_org)))")
 
 # F0 refienment by StoneMask
 f0 = stonemask(w, x, timeaxis, f0)
-f0_refined_org = vec(readdlm("data/f0_refined.txt"))
+f0_refined_org = vec(readdlm(joinpath(dirname(@__FILE__), "data", "f0_refined.txt")))
 
 println("Maximum error in StoneMask is $(maximum(abs(f0-f0_refined_org)))")
 @test length(f0) == length(f0_refined_org)
@@ -30,20 +30,20 @@ println("Maximum error in StoneMask is $(maximum(abs(f0-f0_refined_org)))")
 
 # Spectral envelope estimation by CheapTrick
 spectrogram = cheaptrick(w, x, timeaxis, f0)
-spectrogram_org = readdlm("data/spectrogram.txt")'
+spectrogram_org = readdlm(joinpath(dirname(@__FILE__), "data", "spectrogram.txt"))'
 
 println("Maximum error in CheapTrick is $(maximum(abs(spectrogram - spectrogram_org)))")
 @test size(spectrogram) == size(spectrogram_org)
 @test_approx_eq_eps spectrogram spectrogram_org 1.0e-16
 
 residual = platinum(w, x, timeaxis, f0, spectrogram)
-residual_org = readdlm("data/residual_spectrogram.txt")'
+residual_org = readdlm(joinpath(dirname(@__FILE__), "data", "residual_spectrogram.txt"))'
 println("Maximum error in Platinum is $(maximum(abs(residual-residual_org)))")
 @test size(residual) == size(residual_org)
 @test_approx_eq_eps residual residual_org 1.0e-16
 
 aperiodicity = aperiodicityratio(w, x, f0, timeaxis)
-aperiodicity_org = readdlm("data/aperiodicity.txt")'
+aperiodicity_org = readdlm(joinpath(dirname(@__FILE__), "data", "aperiodicity.txt"))'
 println("Maximum error in aperiodicy is $(maximum(abs(aperiodicity-aperiodicity_org)))")
 @test size(aperiodicity) == size(aperiodicity)
 @test_approx_eq_eps aperiodicity aperiodicity_org 1.0e-16
@@ -51,14 +51,14 @@ println("Maximum error in aperiodicy is $(maximum(abs(aperiodicity-aperiodicity_
 # Synthesis
 y_length = int((length(f0)-1)*period/1000 * fs) + 1
 y = synthesis(w, f0, spectrogram, residual, y_length)
-y_org = vec(readdlm("data/x_synthesized.txt"))
+y_org = vec(readdlm(joinpath(dirname(@__FILE__), "data", "x_synthesized.txt")))
 println("Maximum error in synthesis is $(maximum(abs(y-y_org)))")
 @test length(y) == length(y_org)
 @test_approx_eq_eps y y_org 1.0e-16
 
 # Synthesis from aperiodicy
 y = synthesis_from_aperiodicity(w, f0, spectrogram, aperiodicity, y_length)
-y_org = vec(readdlm("data/x_synthesized_from_aperiodicity.txt"))
+y_org = vec(readdlm(joinpath(dirname(@__FILE__), "data", "x_synthesized_from_aperiodicity.txt")))
 println("Maximum error in synthesis_from_aperiodicy is $(maximum(abs(y-y_org)))")
 @test length(y) == length(y_org)
 @test_approx_eq_eps y y_org 1.0e-16
