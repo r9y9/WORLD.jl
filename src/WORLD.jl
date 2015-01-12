ab30c451177ac4fa512680623716ed4801bdb862
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-01-12 11:37:09 +0900

    abstraction: Vector -> AbstractArray

--- src/WORLD.jl
+++ src/WORLD.jl
@@ -46,51 +46,57 @@ const defaultdioopt = DioOption(80.0, 640.0, 2.0, 5.0, 4)
 # will be deprecated
 World(;fs::Real=44100, period::Float64=5.0) = World(fs, period)
 
-function dio(w::World, x::Vector{Float64}; opt::DioOption=defaultdioopt)
+function dio(w::World, x::AbstractVector{Float64}; opt::DioOption=defaultdioopt)
     w.period == opt.period ||
         throw(ArgmentError("Inconsistent frame period: $(w.period) != $(opt.period)"))
     dio(x, w.fs, opt)
 end
 
-function stonemask(w::World, x::Vector{Float64},
-                   timeaxis::Vector{Float64}, f0::Vector{Float64})
+function stonemask(w::World, x::AbstractVector{Float64},
+                   timeaxis::AbstractVector{Float64},
+                   f0::AbstractVector{Float64})
     stonemask(x, w.fs, timeaxis, f0)
 end
 
-function star(w::World, x::Vector{Float64},
-              timeaxis::Vector{Float64}, f0::Vector{Float64})
+function star(w::World, x::AbstractVector{Float64},
+              timeaxis::AbstractVector{Float64}, f0::AbstractVector{Float64})
     star(x, w.fs, timeaxis, f0)
 end
 
-function cheaptrick(w::World, x::Vector{Float64},
-                    timeaxis::Vector{Float64}, f0::Vector{Float64})
+function cheaptrick(w::World, x::AbstractVector{Float64},
+                    timeaxis::AbstractVector{Float64},
+                    f0::AbstractVector{Float64})
     cheaptrick(x, w.fs, timeaxis, f0)
 end
 
-function platinum(w::World, x::Vector{Float64},
-                  timeaxis::Vector{Float64}, f0::Vector{Float64},
-                  spectrogram::Matrix{Float64})
+function platinum(w::World, x::AbstractVector{Float64},
+                  timeaxis::AbstractVector{Float64},
+                  f0::AbstractVector{Float64},
+                  spectrogram::AbstractMatrix{Float64})
     platinum(x, w.fs, timeaxis, f0, spectrogram)
 end
 
-function synthesis(w::World, f0::Vector{Float64},
-                   spectrogram::Matrix{Float64},
-                   residual::Matrix{Float64}, len::Int)
+function synthesis(w::World, f0::AbstractVector{Float64},
+                   spectrogram::AbstractMatrix{Float64},
+                   residual::AbstractMatrix{Float64},
+                   len::Integer)
     synthesis(f0, spectrogram, residual, w.period,  w.fs, len)
 end
 
-function aperiodicityratio(w::World, x::Vector{Float64},
-                           f0::Vector{Float64},timeaxis::Vector{Float64})
+function aperiodicityratio(w::World, x::AbstractVector{Float64},
+                           f0::AbstractVector{Float64},
+                           timeaxis::AbstractVector{Float64})
     aperiodicityratio(x, w.fs, f0, timeaxis)
 end
 
-function synthesis_from_aperiodicity(w::World, f0::Vector{Float64},
-                                     spectrogram::Matrix{Float64},
-                                     aperiodicity::Matrix{Float64}, len::Int)
+function synthesis_from_aperiodicity(w::World, f0::AbstractVector{Float64},
+                                     spectrogram::AbstractMatrix{Float64},
+                                     aperiodicity::AbstractMatrix{Float64},
+                                     len::Integer)
     synthesis_from_aperiodicity(f0, spectrogram, aperiodicity, w.period,
                                 w.fs, len)
 end
 
-@deprecate dio1(w::World, x::Vector{Float64}) dio(w, x, opt=DioOption(80.0, 640.0, 2.0, w.period, div(w.fs, 4000.0)))
+@deprecate dio1(w::World, x::AbstractVector{Float64}) dio(w, x, opt=DioOption(80.0, 640.0, 2.0, w.period, div(w.fs, 4000.0)))
 
 end # module WORLD
